e473b1516ef313a73520de0fbc5842f974430226
davidp0717 <72396483+davidp0717@users.noreply.github.com>
2026-01-17 08:32:28 -0500

    Update test7.2.py

--- test7.2.py
+++ test7.2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from scipy.stats import t
 
-def fit_t_mle():
+def fit_t():
     df = pd.read_csv("/Users/panzhouzhen/Documents/FinTech-545-Fall2025/testfiles/data/test7_2.csv")
     x = df["x1"].to_numpy(dtype=float)
 
@@ -11,4 +11,4 @@ def fit_t_mle():
     print("s  =", float(s))
     print("nu =", float(nu))
 
-fit_t_mle()
+fit_t()
